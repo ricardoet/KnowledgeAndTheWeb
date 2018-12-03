eb7ed5de0a41e19884956e473d6aa5270baacbe2
Ricardo Elizondo <ricardo.elizondo.t@gmail.com>
2018-12-03 22:46:37 +0100

    Basic first version now asks the user for the label and creates a list of objects with pair sentence-label

--- sentence divider.py
+++ sentence divider.py
@@ -4,11 +4,26 @@ Created on Mon Dec  3 17:34:56 2018
 
 @author: ricar
 """
+class labeled_sentence:
+    def __init__(self, sentence, label):
+        self.sentence = sentence
+        self.label = label
 
 file = open("input_oct16.txt", "r")
 mat_lines = file.readlines()
 
+list_labeled_sentence = []
+
 for line in mat_lines:
     line = line.split(':')[-1]
     line = line.split('.')[:-1]
-    print (line)
+    
+    for separate_line in line:1
+        if separate_line.count(' ') > 60:
+            print (separate_line)
+            label = input("What is the label for this? 1-4")
+            ##HACER CHEQUEOS
+            pair = labeled_sentence(separate_line, label)
+            list_labeled_sentence.append(pair)
+
+#list_labeled_sentence es una lista de objetos 'labeled_sentence'
