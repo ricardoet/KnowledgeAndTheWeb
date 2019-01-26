b959847d1550df2682886174c354c471e801fa01
Ricardo Elizondo <ricardo.elizondo.t@gmail.com>
2019-01-26 15:56:34 +0100

    Fixed bug and enhanced functionality

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -4,24 +4,32 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import normalize
 import numpy as np
 
+def print_confusion(array, names):
+        df_cm = pd.DataFrame(array, index = [i for i in names], 
+                             columns = [i for i in names])
+    
+        plt.figure(figsize = (10,7))
+        sn.heatmap(df_cm, annot=True, cmap="YlGnBu")
+
+def confusion_total(array, names):
+    print_confusion(array, names)
+    
+def confusion_relative(array, names):
+        for row in range(len(array)):
+            total = sum(array[row])
+            for num in range(len(array[row])):
+                array[row][num] = array[row][num] / total
+                
+        print_confusion(array, names)
+
+
 array_final = [[0, 2, 3, 7], 
-        [1,7,5,11], 
-        [0,1,12,32], 
-        [0,6,11,289]]
+               [1,7,5,11], 
+               [0,1,12,32], 
+               [0,6,11,289]]
 
-array_inicial = [[0, 1, 5, 6], 
-        [2, 4, 5, 13], 
-        [0, 0, 14, 31], 
-        [0, 8, 16, 282]]
-
-array_inicial = [[8,28],
-                 [7,344]]
-
-array_final = [[13,23],
-                 [7,344]]
-
-array = [[0,0],
-                 [0,0]]
+array_final = [[1,1],
+              [1,1]]
 
 for row in range(len(array_final)):
     for num in range(len(array_final)):
@@ -33,7 +41,4 @@ for row in range(len(array)):
             array[row][num] = array[row][num] / sum(array_final[row])
         
 
-df_cm = pd.DataFrame(array, index = [i for i in ["CS", "US"]],
-                  columns = [i for i in ["CS", "US"]])
-plt.figure(figsize = (10,7))
-sn.heatmap(df_cm, annot=True)
+confusion_relative(array_final, ["CS", "US"])
